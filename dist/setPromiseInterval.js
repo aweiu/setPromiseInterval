e47c3c0568fa8c915c378a153de8d17eb8ec2b78
aweiu <328080339@qq.com>
2019-06-03 19:49:32 +0800

    build from v1.0.2

--- dist/setPromiseInterval.js
+++ dist/setPromiseInterval.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const ids = {};
+const ids = new Set();
 let id = 0;
 function delay(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 async function run(id, ...[handler, interval = 0]) {
     while (true) {
-        if (!ids.hasOwnProperty(id)) {
+        if (!ids.has(id)) {
             break;
         }
         const startTime = new Date().getTime();
@@ -16,12 +16,14 @@ async function run(id, ...[handler, interval = 0]) {
     }
 }
 function clearPromiseInterval(intervalId) {
-    delete ids[intervalId];
+    if (typeof intervalId === 'number') {
+        ids.delete(intervalId);
+    }
 }
 exports.clearPromiseInterval = clearPromiseInterval;
 function setPromiseInterval(handler, interval) {
     id += 1;
-    ids[id] = null;
+    ids.add(id);
     run(id, handler, interval);
     return id;
 }
--- types/setPromiseInterval.d.ts
+++ types/setPromiseInterval.d.ts
@@ -1,2 +1,2 @@
-export declare function clearPromiseInterval(intervalId: number): void;
+export declare function clearPromiseInterval(intervalId?: number): void;
 export default function setPromiseInterval(handler: (...args: any[]) => Promise<any>, interval?: number): number;
