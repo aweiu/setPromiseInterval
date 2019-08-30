b6d67e449faf332d800e841bcdc687d277ab802c
aweiu <328080339@qq.com>
2019-08-30 19:47:03 +0800

    修复连续多次 clearPromiseInterval，只有最后一个可以 resolve 的 bug

--- src/set-promise-interval.ts
+++ src/set-promise-interval.ts
@@ -1,4 +1,7 @@
-const clearResolves = new Map<number, null | (() => any)>()
+const clearTasks = new Map<
+  number,
+  null | { promise: Promise<any>; resolve: () => any }
+>()
 let id = 0
 
 function delay(ms: number) {
@@ -10,10 +13,10 @@ async function run(
   ...[handler, interval = 0]: Parameters<typeof setPromiseInterval>
 ) {
   while (true) {
-    const clearResolve = clearResolves.get(id)
-    if (clearResolve) {
-      clearResolves.delete(id)
-      clearResolve()
+    const task = clearTasks.get(id)
+    if (task) {
+      clearTasks.delete(id)
+      task.resolve()
       break
     }
     const startTime = new Date().getTime()
@@ -23,9 +26,18 @@ async function run(
 }
 
 export function clearPromiseInterval(intervalId?: number) {
-  return typeof intervalId === 'number' && clearResolves.has(intervalId)
-    ? new Promise((resolve) => clearResolves.set(intervalId, resolve))
-    : Promise.resolve()
+  if (typeof intervalId === 'number' && clearTasks.has(intervalId)) {
+    const task = clearTasks.get(intervalId)
+    if (!task) {
+      let _resolve
+      const promise = new Promise((resolve) => (_resolve = resolve))
+      // @ts-ignore
+      clearTasks.set(intervalId, { resolve: _resolve, promise })
+    }
+    // @ts-ignore
+    return clearTasks.get(intervalId).promise
+  }
+  return Promise.resolve()
 }
 
 export default function setPromiseInterval(
@@ -33,7 +45,7 @@ export default function setPromiseInterval(
   interval?: number,
 ) {
   id += 1
-  clearResolves.set(id, null)
+  clearTasks.set(id, null)
   run(id, handler, interval)
   return id
 }
